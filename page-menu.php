22a0dadd551d7ab3218e45a2d1b7c56eae3e93c3
Sam Screpnek <118566100+samscrepnek@users.noreply.github.com>
2023-03-21 15:48:28 -0700

    Update page-menu.php

--- page-menu.php
+++ page-menu.php
@@ -17,28 +17,77 @@ get_header();
 
 	<main id="primary" class="site-main">
 
+		<header class="entry-header">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		</header><!-- .entry-header -->
+
+		<div class="menu-nav">
+			<p><a href="#menu-wine">Wine</a></p>
+			<p><a href="#menu-food">Food</a></p>
+		</div>
+
 		<?php
 		$args = array(
-			'post_type' => 'bon-vin-locations',
+			'post_type' => 'product',
 			'posts_per_page' => -1,
+			'tax_query'      => array(
+				array(
+					'taxonomy' => 'bon-vin-menu-items',
+					'field'    => 'slug',
+					'terms'    => 'wine'
+				),
+			)
 		);
 		$query = new WP_QUERY( $args );
 		if ( $query -> have_posts() ) :
 			?>
-			<div class="navigation-div">
+			<div class="menu" id="menu-wine">
 			<?php
 			while ( $query -> have_posts() ) :
 				$query -> the_post();
 				$id = get_the_ID();
+				$product = wc_get_product($id);
+				$product_price = $product->get_price();
 				?>
-				<a href="#<?php echo $id; ?>"><?php the_title(); ?></a>
-				<?php
-				if ( get_field( 'single_location' ) ) : ?>
-					<h2><?php the_field( 'single_location' ); ?></h2>
-					<?php
-				endif;
+				<div class="menu-item">
+					<p><?php the_title(); ?></p>
+					<p><?php echo $product_price ?></p>
+				</div>
+				<?php		
+			endwhile;
+			wp_reset_postdata();
+			?>
+			</div>
+			<?php
+		endif;
 
-					
+		$args = array(
+			'post_type' => 'product',
+			'posts_per_page' => -1,
+			'tax_query'      => array(
+				array(
+					'taxonomy' => 'bon-vin-menu-items',
+					'field'    => 'slug',
+					'terms'    => 'food'
+				),
+			)
+		);
+		$query = new WP_QUERY( $args );
+		if ( $query -> have_posts() ) :
+			?>
+			<div class="menu" id="menu-food">
+			<?php
+			while ( $query -> have_posts() ) :
+				$query -> the_post();
+				$id = get_the_ID();
+				$product = wc_get_product($id);
+				$product_price = $product->get_price();
+				?>
+				<div class="menu-item">
+					<p><?php the_title(); ?></p>
+					<p><?php echo $product_price ?></p>
+				</div>
+				<?php		
 			endwhile;
 			wp_reset_postdata();
 			?>
