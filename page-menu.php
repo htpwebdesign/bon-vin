924b12115538b3d2a076184688f3131f1d52b1ba
Sam Screpnek <118566100+samscrepnek@users.noreply.github.com>
2023-03-21 13:39:58 -0700

    Update page-menu.php

--- page-menu.php
+++ page-menu.php
@@ -18,21 +18,36 @@ get_header();
 	<main id="primary" class="site-main">
 
 		<?php
-		while ( have_posts() ) :
-			the_post();
+		$args = array(
+			'post_type' => 'bon-vin-locations',
+			'posts_per_page' => -1,
+		);
+		$query = new WP_QUERY( $args );
+		if ( $query -> have_posts() ) :
+			?>
+			<div class="navigation-div">
+			<?php
+			while ( $query -> have_posts() ) :
+				$query -> the_post();
+				$id = get_the_ID();
+				?>
+				<a href="#<?php echo $id; ?>"><?php the_title(); ?></a>
+				<?php
+				if ( get_field( 'single_location' ) ) : ?>
+					<h2><?php the_field( 'single_location' ); ?></h2>
+					<?php
+				endif;
 
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
+					
+			endwhile;
+			wp_reset_postdata();
+			?>
+			</div>
+			<?php
+		endif;
 		?>
 
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
