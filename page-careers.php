d378db85f1b91f467f5d7f5224bf18e71acfc9e7
vanessa-liang <118566123+vanessa-liang@users.noreply.github.com>
2023-03-22 12:03:32 -0700

    careers page and single job page

--- page-careers.php
+++ page-careers.php
@@ -23,16 +23,62 @@ get_header();
 
 			get_template_part( 'template-parts/content', 'page' );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
 		endwhile; // End of the loop.
 		?>
+
+<?php
+	
+	$taxonomy = 'bon-vin-career-locations';
+	$terms = get_terms( 
+		array(
+			'taxonomy' => $taxonomy
+		) 
+	);
+
+	if ( $terms && ! is_wp_error( $terms ) ) {
+		foreach ( $terms as $term ) {
+
+			$args = array(
+				'post_type'      => 'bon-vin-careers',
+				'posts_per_page' => -1,
+				'order'          => 'ASC',
+				'orderby'        => 'title',
+				'tax_query'      => array(
+					array(
+						'taxonomy' => $taxonomy,
+						'field'    => 'slug',
+						'terms'    => $term->slug,
+					)
+				),
+			);
+			$query = new WP_Query ($args);
+			if ($query -> have_posts()) {
+				echo '<h2>' . esc_html( $term->name ) . '</h2>';
+				while ($query -> have_posts()) {
+					$query -> the_post();
+					?>
+					<section>
+					<?php
+					if ( function_exists( 'get_field' ) ) {
+						if ( get_field( 'job_title' ) ) {
+							?>
+							<h3><?php the_field( 'job_title' ); ?></h3>
+							<p><?php the_field('job_overview') ?></p>
+							<a href="<?php the_permalink(); ?>">Details</a>
+							<?php
+						}
+					}
+					?>
+					</section>
+					<?php
+				}
+			}
+		}
+		wp_reset_postdata();
+	}
+	?>
 
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
