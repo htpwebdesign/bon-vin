8bba2c9e2e2d795bc3fd2c384401c69cd2e9721f
vanessa-liang <118566123+vanessa-liang@users.noreply.github.com>
2023-03-27 15:35:01 -0700

    corrected quotation marks

--- single-bon-vin-locations.php
+++ single-bon-vin-locations.php
@@ -15,7 +15,7 @@ get_header();
 				the_post();
 		?>
 		<header class="entry-header">
-            <?php the_title( "<h1 class=“entry-title”>", "</h1>" ); ?>
+            <?php the_title( "<h1 class='entry-title'>", "</h1>" ); ?>
         </header><!-- .entry-header -->
 
 		<section class='main-image-location'>
