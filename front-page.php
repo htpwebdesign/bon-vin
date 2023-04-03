064533502f4920e06e86a6691260ab3f21f09308
Lily Colby <117856062+lilycolby@users.noreply.github.com>
2023-04-02 23:13:44 -0700

    commit to about and front

--- front-page.php
+++ front-page.php
@@ -36,7 +36,6 @@ get_header();
 				<?php
 				}
 				?>	
-
 				<section class="shop-cta">
 					<?php
 					if ( get_field('shop_cta') ) {
@@ -51,6 +50,7 @@ get_header();
 					}
 					?>
 				</section>
+				<div class= "content-wrapper">	
 
 				<section class="wine-list-title">
 					<?php
@@ -70,6 +70,33 @@ get_header();
 					?>
 				</section> 
 
+				</div>
+
+				<section class="wine-list">
+				<?php
+				$featured_posts = get_field('wine_list');
+				if( $featured_posts ): ?>
+					
+					<?php foreach( $featured_posts as $featured_post ): 
+						$permalink = get_permalink( $featured_post->ID );
+						$title = get_the_title( $featured_post->ID );
+						$custom_field = get_field( 'field_name', $featured_post->ID );
+					?>
+
+					<section class="single-wine">
+						<!-- <a href="<?php echo esc_url( $permalink ); ?>"><?php echo esc_html( $title ); ?></a> -->
+						<?php echo get_the_post_thumbnail( $featured_post->ID, 'medium',); ?>
+						<div class="flexed-wine">
+							<h3><?php echo esc_html( $title ); ?></h3>
+							<p><?php echo get_the_content( "", false, $featured_post->ID ); ?> </p>
+						</div>
+						
+						<a href="<?php echo esc_url( $permalink ); ?>">Shop</a>
+					</section>
+						
+					<?php endforeach; ?>
+				<?php endif; ?>
+				</section>
 				<section class="shop-all-cta">
 					<?php 
 					if ( get_field('shop_cta') ) {
@@ -84,29 +111,7 @@ get_header();
 					}
 					?>
 				</section>
-
-				<section class="wine-list">
-				<?php
-				$featured_posts = get_field('wine_list');
-				if( $featured_posts ): ?>
-					
-					<?php foreach( $featured_posts as $featured_post ): 
-						$permalink = get_permalink( $featured_post->ID );
-						$title = get_the_title( $featured_post->ID );
-						$custom_field = get_field( 'field_name', $featured_post->ID );
-					?>
-
-					<section>
-						<!-- <a href="<?php echo esc_url( $permalink ); ?>"><?php echo esc_html( $title ); ?></a> -->
-						<h3><?php echo esc_html( $title ); ?></h3>
-						<?php echo get_the_content( "", false, $featured_post->ID ); ?> 
-						<?php echo get_the_post_thumbnail( $featured_post->ID, 'medium',); ?>
-						<a href="<?php echo esc_url( $permalink ); ?>">Shop</a>
-					</section>
-						
-					<?php endforeach; ?>
-				<?php endif; ?>
-				</section>
+				
 
 			<?php
 
--- page-about.php
+++ page-about.php
@@ -15,7 +15,7 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main id="primary" class="site-main about-main content-wrapper">
 	<header class="about-title">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header>
