bce50fd7bbbe9b295b79468b3a482645fb3d34e7
Sam Screpnek <118566100+samscrepnek@users.noreply.github.com>
2023-04-05 17:18:56 -0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -304,3 +304,11 @@ function bon_vin_tutorial_two_render() {
 	<?php
 }
 
+//admin menu bar
+function post_remove ()      //creating functions post_remove for removing menu item
+{ 
+   remove_menu_page('edit.php');
+   remove_menu_page( 'edit-comments.php' );
+}
+
+add_action('admin_menu', 'post_remove');   //adding action for triggering function call
